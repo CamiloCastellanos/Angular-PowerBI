12bbad88aae7cde168e8fdf8ecef908a7eed8fdb
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2021-03-25 18:56:17 -0500

    Creacion de Rutas

--- AngularPowerBI/src/app/app-routing.module.ts
+++ AngularPowerBI/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PowerBILibraryComponent } from './components/power-bilibrary/power-bilibrary.component';
+import { PowerBILinkComponent } from './components/power-bilink/power-bilink.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'Link', component:PowerBILinkComponent},
+  {path:'Library', component:PowerBILibraryComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'Link' },
+  { path: '', pathMatch: 'full', redirectTo: 'Link' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
