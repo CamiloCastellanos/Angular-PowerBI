ce6a50f3408eccf2eb66cbd5ae6b0c3b12259ada
CamiloCastellanos <camilo.castellanos.puentes@gmail.com>
2021-06-21 21:24:16 -0500

    Actualizacion de menu lateral

--- AngularPowerBI/src/app/components/navbar/navbar.component.ts
+++ AngularPowerBI/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Dashboard } from 'src/app/Model/Dashboard.model';
+import { DashboardService } from '../../services/dashboard.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  public listaDashboard: Dashboard[] = [];
+
+  constructor(private dashboardService: DashboardService) {
+    this.obtenerDashboard();
+  }
 
   ngOnInit(): void {
+    this.obtenerDashboard();
+  }
+
+  private obtenerDashboard() {
+    this.listaDashboard = this.dashboardService.listaDashboard;
   }
 
 }
